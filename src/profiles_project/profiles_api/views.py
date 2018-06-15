40951277d66dfc375528e73bac59d955a90cfb39
Ana Castellanos <ana.lec15@gmail.com>
2018-06-15 12:52:31 -0500

    Added put, patch, delete.

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+
 #Importa los serializers
 from . import serializers
+from rest_framework import status
 
 # Create your views here.
 
@@ -26,7 +27,33 @@ class HelloApiView(APIView):
 
         #Regresamos un objeto de respuesta en un diccionario que se
         #convierte a JSON
-        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+        return Response({'message': 'Hola!', 'an_apiview': an_apiview})
 
     def post(self, request):
         """Crea un mensaje de bienvenida con nuestro nombre"""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}!'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    #PK significa clave primaria a un objeto e especifico.
+    def put(self, request, pk=None):
+        """Maneja el actualizado de un objeto"""
+
+        return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+        """Patch sólo actualiza los campos requeridos."""
+
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+        """Borra objetos"""
+
+        return Response({'method': 'delete'})
